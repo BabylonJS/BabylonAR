8592380b162e52809c3221ec3639a1c0baa2dff1
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2020-03-25 12:59:44 -0700

    Starting work on example for imageTemplateTracker.

--- src/ts/shared/constants.ts
+++ src/ts/shared/constants.ts
@@ -1,5 +1,7 @@
-const CDN_URL: string = "https://ar.babylonjs.com/";
+// const CDN_URL: string = "https://ar.babylonjs.com/";
+const CDN_URL: string = "http://127.0.0.1:8080";
 const CDN_WASM_MODULES_URL: string = CDN_URL + "wasm/";
 
 export const EXAMPLE_MODULE_URL: string = CDN_WASM_MODULES_URL + "webpiled-aruco-ar.js";
 export const ARUCO_META_MARKER_TRACKER_URL: string = CDN_WASM_MODULES_URL + "aruco-meta-marker-tracker.js";
+export const IMAGE_TEMPLATE_TRACKER_URL: string = CDN_WASM_MODULES_URL + "image-template-tracker.js";
